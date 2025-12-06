9e1e995999a0b0e16eb411298b5e0eaef96ebbd1
milkstack <landonkeller.dev@gmail.com>
2025-12-05 23:15:02 -0800

    6

--- day-6/index2.ts
+++ day-6/index2.ts
@@ -5,19 +5,19 @@ const main = () => {
     .split("\n")
     .filter((_) => _ !== "");
 
-  const columns: { width: number; operator: "+" | "*" }[] = [];
+  const problems: { width: number; operator: "+" | "*" }[] = [];
 
   const numbersRows = input.slice(0, input.length - 1);
   const operatorsRow = input[input.length - 1];
 
-  let currentWidth = 1;
-
+  //   use the row of operators to build a set of "problems" of shape {width: number, operator: '+' | '*'}
+  let currentWidth = 1; // start at 1 -- solves off by one error with first problem not having a space before
   operatorsRow
     .split("")
     .reverse()
     .forEach((char, index) => {
       if (char === "+" || char === "*") {
-        columns.unshift({ width: currentWidth, operator: char });
+        problems.unshift({ width: currentWidth, operator: char });
         currentWidth = 0;
         return;
       }
@@ -28,32 +28,32 @@ const main = () => {
   let total = 0;
   let cursor = 0;
 
-  columns.forEach((column) => {
+  problems.forEach((problem) => {
     let localTotal = 0;
 
-    //   ------------2----------------
-    for (let i = 0; i < column.width; i++) {
+    // for each single column of characters within a problem
+    for (let i = 0; i < problem.width; i++) {
       const index = cursor + i;
-      let numberString = "";
+      let numberString = ""; // build a number
 
-      //   -----------3-----------------
+      // for each row, still a long string 'xxx xx xxxx '
+      // get character at  index
       for (let row of numbersRows) {
         const char = row[index];
         if (char !== "") numberString += char;
       }
-      //   -----------/3-----------------
 
       const number = Number(numberString);
 
-      if (localTotal === 0 || column.operator === "+") {
+      if (localTotal === 0 || problem.operator === "+") {
         localTotal += number;
       } else {
         localTotal *= number;
       }
     }
-    cursor += column.width + 1;
 
-    //   ----------/2------------------
+    // add the problem's width to our cursor, add the answer from the problem and repeat
+    cursor += problem.width + 1;
 
     total += localTotal;
   });
